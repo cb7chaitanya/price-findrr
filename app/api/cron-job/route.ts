2fe89ea48dac0590248c05d844789fd419b3cc8b
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-06-03 16:35:25 +0530

    [FIX] Fixing maxDuration Property for Cron route

--- app/api/cron-job/route.ts
+++ app/api/cron-job/route.ts
@@ -6,7 +6,7 @@ import { setEmailBody, sendEmail } from "../../../server/mailService"
 import { NextResponse } from "next/server"
 
 
-export const maxDuration = 300
+export const maxDuration = 60
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
